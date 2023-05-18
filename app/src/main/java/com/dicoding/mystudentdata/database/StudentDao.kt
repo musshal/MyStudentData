5bcec0502165b9426bc161e1e6be005b425c2f5b
Musthafa Faishal <musthafafaishal@gmail.com>
2023-05-18 12:11:05 +0700

    create: rawQuery

---
+++ app/src/main/java/com/dicoding/mystudentdata/database/StudentDao.kt
@@ -0,0 +1,38 @@
+package com.dicoding.mystudentdata.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import androidx.sqlite.db.SupportSQLiteQuery
+
+@Dao
+interface StudentDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertStudent(student: List<Student>)
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertUniversity(university: List<University>)
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertCourse(course: List<Course>)
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertCourseStudentCrossRef(courseStudentCrossRef: List<CourseStudentCrossRef>)
+
+    @Query("SELECT * from student")
+    fun getAllStudent(): LiveData<List<Student>>
+
+    @RawQuery(observedEntities = [Student::class])
+    fun getAllStudent(query: SupportSQLiteQuery): LiveData<List<Student>>
+
+    @Transaction
+    @Query("SELECT * from student")
+    fun getAllStudentAndUniversity(): LiveData<List<StudentAndUniversity>>
+
+    @Transaction
+    @Query("SELECT * from university")
+    fun getAllUniversityAndStudent(): LiveData<List<UniversityAndStudent>>
+
+    @Transaction
+    @Query("SELECT * from student")
+    fun getAllStudentWithCourse(): LiveData<List<StudentWithCourse>>
+}
